a750c55b6492c44671cc84c7af16715fd5a950cc
mathis le vu <levu.mathis56@gmail.com>
2024-02-05 23:57:38 +0100

    Move importManager to importData and to remove importData folder

---
+++ data/importManager.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+class importManager:
+
+    def __init__(self) -> None:
+        pass
+    
+    def spots(self, tickers : list, period="5Y", fields = ["Open", "High", "Close", "Low", "Close", "Volume", "Dividends", "Stock Splits"])->pd.DataFrame:
+
+        data = pd.DataFrame()
+        stock_split_asked : bool = "Stock Splits" in fields
+        
+        if any([field not in ["Open", "High", "Close", "Low", "Close", "Volume", "Dividends", "Stock Splits"] for field in fields]):
+            raise ValueError("Invalid input : fields not found in Histo DataFrame")
+                
+        for ticker in tickers:
+            histo = yf.Ticker(ticker).history(period=period)[fields]
+            histo["ticker"] = ticker
+            if stock_split_asked: 
+                histo["Stock Splits"] = histo["Stock Splits"].astype(dtype=int)
+            
+            if data.empty:
+                data = histo
+            else:
+                data = data.append(histo)
+        return data
+        
+def test():
+    im = importManager()
+    im.spots(["MSFT"])
+    
+
+if __name__ == "__main__":
+    test()

b02cd21e4b5ea2e412f7a0f061a26c3a7e7d599a
mathis le vu <levu.mathis56@gmail.com>
2024-02-06 23:40:47 +0100

    Add global market index universe

--- data/universeManager.py
+++ data/universeManager.py
@@ -5,11 +5,18 @@ import yfinance as yf
 import requests
 from requests_html import HTMLSession
 
-
 class universeManager:
     
     def __init__(self) -> None:
         pass
+    
+    def global_market_index(self)->list:
+        session = HTMLSession()
+        num_currencies=250
+        resp = session.get(f"https://fr.finance.yahoo.com/indices-mondiaux/?offset=0&count=250={num_currencies}")
+        tables = pd.read_html(resp.html.raw_html)               
+        df = tables[0].copy()
+        return df.Symbole.tolist()
     
     def crypto(self)->list:
         session = HTMLSession()
@@ -21,7 +28,8 @@ class universeManager:
     
 def test():
     um = universeManager()
-    um.crypto()
+    tickers = um.global_market_index()
+    return tickers
 
 if __name__ == "__main__":
     test()
